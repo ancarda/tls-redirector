75e8196fd1fe30aee79baa4eab4ed9b655a8f697
Mark Dain <mark@markdain.net>
2020-07-30 21:00:24 +0100

    Break up main.go into two files
    This commit makes main.go a bit smaller by moving the HTTP specific
    logic out to a separate file.

---
+++ http.go
@@ -0,0 +1,56 @@
+// Package main implements tls-redirector, a microservice that
+// is designed to forward all HTTP traffic to HTTPS.
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "tls-redirector/2.1")
+
+	// If we haven't been given a host, just abort.
+	if r.Host == "" {
+		http.Error(w, "tls-redirector cannot handle this request because no `host` header was sent by your browser.", http.StatusBadRequest)
+		return
+	}
+
+	// If we've seen a direct request, such as an IP address,
+	// we'll discard it now. Not only is it uncommon to see
+	// X.509 certificates for IP addresses, it's very likely
+	// to be junk traffic (scanning bots). We can save the
+	// real web server some effort by dropping it now.
+	if isIPAddress(r.Host) {
+		http.Error(w, "tls-redirector cannot redirect IP addresses.",
+			http.StatusBadRequest)
+		return
+	}
+
+	// If we are serving the ACME HTTP challenges, handle that here.
+	if acmeChallengeDir != "" {
+		if strings.HasPrefix(r.URL.Path, acmeChallengeUrlPrefix) {
+			id := strings.TrimPrefix(r.URL.Path, acmeChallengeUrlPrefix)
+			w.Header().Set("Content-Type", "text/plain")
+			b, err := ioutil.ReadFile(acmeChallengeDir + "/" + id)
+			if err != nil {
+				http.Error(w, "File Not Found", http.StatusNotFound)
+				return
+			}
+
+			w.Write(b)
+			return
+		}
+	}
+
+	// Overwrite the scheme to https:// and redirect.
+	// Change host as well as in r.URL, it's empty.
+	r.URL.Host = r.Host
+	r.URL.Scheme = "https"
+	http.Redirect(w, r, r.URL.String(), http.StatusMovedPermanently)
+}
+
+func init() {
+	http.HandleFunc("/", handle)
+}
