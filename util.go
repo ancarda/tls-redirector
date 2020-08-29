fd741188caf93e7e5b811cab8f1f153cd456a54b
Mark Dain <mark@markdain.net>
2020-08-29 16:47:02 +0100

    Improve detection of IPv6 addresses
    This commit strips the square brackets that IPv6 addresses can be in when a
    port number is suffixed on the end. The presence of these square brackets
    would cause the `net.ParseIP' function to (correctly) determine the string to
    not be an IP address.
    
    If an IPv6 address with a port number is sent, it is already handled because
    the `net.SplitHostPort' function is able to detect that. This commit fixes the
    edge case where no port number is present, but square brackets are.
    
    Issue: https://todo.sr.ht/~ancarda/tls-redirector/4

--- util.go
+++ util.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"net"
+	"strings"
 )
 
+func removeSquareBrackets(s string) string {
+	if !strings.HasPrefix(s, "[") {
+		return s
+	}
+
+	if !strings.HasSuffix(s, "]") {
+		return s
+	}
+
+	return s[1 : len(s)-1]
+}
+
 func isIPAddress(host string) bool {
-	if net.ParseIP(host) != nil {
+	// Addresses without a port number attached can be checked as-is
+	if net.ParseIP(removeSquareBrackets(host)) != nil {
 		return true
 	}
 
+	// Try to strip the port number off, then check again.
 	if host, _, err := net.SplitHostPort(host); err == nil {
-		if net.ParseIP(host) != nil {
+		if net.ParseIP(removeSquareBrackets(host)) != nil {
 			return true
 		}
 	}
--- util_test.go
+++ util_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestRemoveSquareBrackets(t *testing.T) {
+	assert.Equal(t, "[::1]a", removeSquareBrackets("[::1]a"))
+	assert.Equal(t, "a[::1]", removeSquareBrackets("a[::1]"))
+	assert.Equal(t, "::1", removeSquareBrackets("[::1]"))
+}
+
 func TestIsIPAddress_IPv4(t *testing.T) {
 	assert.True(t, isIPAddress("127.0.0.1"), "IPv4 address")
 	assert.True(t, isIPAddress("127.0.0.1:8080"), "IPv4 address with port")
@@ -13,6 +19,7 @@ func TestIsIPAddress_IPv4(t *testing.T) {
 
 func TestIsIPAddress_IPv6(t *testing.T) {
 	assert.True(t, isIPAddress("::1"), "IPv6 address")
+	assert.True(t, isIPAddress("[::1]"), "IPv6 address inside brackets")
 	assert.True(t, isIPAddress("[::1]:8080"), "IPv6 address with port")
 }
 
